11044d758068f4d3a3a9b02210a94ade3b2fed67
geralex <notmylogin@gmail.com>
2019-07-04 19:57:34 +0300

    calendar added

--- src/app/shared/moment.pipe.ts
+++ src/app/shared/moment.pipe.ts
@@ -2,7 +2,8 @@ import {Pipe, PipeTransform} from "@angular/core";
 import * as moment from 'moment'
 
 @Pipe({
-  name: 'moment'
+  name: 'moment',
+  pure: false
 })
 export class MomentPipe implements PipeTransform{
   transform(m: moment.Moment, format: string = 'MMMM YYYY'): string {
